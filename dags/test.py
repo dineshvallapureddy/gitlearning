c28e1272249bfd97c262ca7f7e1b9246eb8e40ca
vallad3 <dineshvallapuredddy@gmail.com>
2021-10-05 18:34:35 +0530

    testing

--- dags/test.py
+++ dags/test.py
@@ -1,11 +1,48 @@
-from airflow.operators.dummy import DummyOperator
-from airflow.operators.bash import BashOperator
-# The DummyOperator is a task and does nothing   
-accurate = DummyOperator(
-task_id='accurate'
+from airflow.example_dags.subdags.subdag import subdag
+from airflow.models import DAG
+from airflow.operators.dummy_operator import DummyOperator
+from airflow.operators.subdag_operator import SubDagOperator
+from airflow.utils.dates import days_ago
+
+DAG_NAME = 'example_subdag_operator'
+
+args = {
+    'owner': 'Airflow',
+    'start_date': days_ago(2),
+}
+
+dag = DAG(
+    dag_id=DAG_NAME,
+    default_args=args,
+    schedule_interval="@once",
+    tags=['example']
 )
-# The BashOperator is a task to execute a bash command
-commands = BashOperator(
-task_id='commands'
-bash_command='sleep 5'
+
+start = DummyOperator(
+    task_id='start',
+    dag=dag,
 )
+
+section_1 = SubDagOperator(
+    task_id='section-1',
+    subdag=subdag(DAG_NAME, 'section-1', args),
+    dag=dag,
+)
+
+some_other_task = DummyOperator(
+    task_id='some-other-task',
+    dag=dag,
+)
+
+section_2 = SubDagOperator(
+    task_id='section-2',
+    subdag=subdag(DAG_NAME, 'section-2', args),
+    dag=dag,
+)
+
+end = DummyOperator(
+    task_id='end',
+    dag=dag,
+)
+
+start >> section_1 >> some_other_task >> section_2 >> end
