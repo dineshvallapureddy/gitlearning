f3241c4827c3d7912ae4dc420a3774edb7ab8cce
vallad3 <dineshvallapuredddy@gmail.com>
2021-10-06 15:15:40 +0530

    testing

--- dags/kube_test.py
+++ dags/kube_test.py
@@ -16,7 +16,7 @@ dag = DAG(
     dag_id=DAG_NAME,
     default_args=args,
     schedule_interval="@once",
-    tags=['example']
+    tags=['test']
 )
 
 quay_k8s = KubernetesPodOperator(
@@ -25,10 +25,13 @@ quay_k8s = KubernetesPodOperator(
     #image_pull_secrets=[k8s.V1LocalObjectReference('testquay')],
     cmds=["bash", "-cx"],
     arguments=["echo", "10", "echo pwd"],
-    labels={"foo": "bar"},
     name="airflow-private-image-pod",
     is_delete_operator_pod=True,
     in_cluster=True,
     task_id="task-two",
     get_logs=True,
     )
+    
+    
+quay_k8s
+    
