8d5b47aab88837c43729dee54a3026805cb1a031
vallad3 <dineshvallapuredddy@gmail.com>
2021-10-18 19:46:13 +0530

    adding new dag

--- dags/test_dih.py
+++ dags/test_dih.py
@@ -34,11 +34,11 @@ default_args = {
     'owner': dset["owner"],
     #'depends_on_past': dset["depends_on_past"],
     'start_date': datetime.utcnow(),
-    'email': dset["email"],
-    'email_on_failure': dset["email_on_failure"],
-    'email_on_retry': dset["email_on_retry"],
-    'concurrency' : dset["concurrency"],
-    'retries': dset["retries"],
+    #'email': dset["email"],
+    #'email_on_failure': dset["email_on_failure"],
+    #'email_on_retry': dset["email_on_retry"],
+    #'concurrency' : dset["concurrency"],
+    #'retries': dset["retries"],
     'retry_delay': timedelta(minutes=dset["retry_delay"])
 }
 
