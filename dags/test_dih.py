cf38cc6eb0e875d48be28d8cb52d9f3e1b152bbd
vallad3 <dineshvallapuredddy@gmail.com>
2021-10-19 14:08:48 +0530

    adding new dag

--- dags/test_dih.py
+++ dags/test_dih.py
@@ -81,6 +81,6 @@ with DAG(dset["name"], default_args=default_args, schedule_interval=dset["schedu
                     taskname = "SF_{}_{}".format(dbname, tabname)
                     taskid = 'TA_' + taskname
                     commands = "{};echo {} | kinit {}@{} && ssh -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes {}@{} '{}'".format(password,
-                        kinitprincipal, kinitdomain, kinitprincipal, edgenodehost, "{} {} {}".format(expo,commands, tabname, dbname))
+                        kinitprincipal, kinitdomain, kinitprincipal, edgenodehost, "{} {} {}".format(expo,scriptpaths["snowexp"], tabname, dbname))
                     ssh_dih = getpodoperator(namespace, image, command, labels, taskname , taskid)
                     ssh_dih
