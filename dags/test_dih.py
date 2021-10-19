c7470bd148ce2af6a45fa5f8bff1f23aa6a55889
dineshvallapureddy <42171134+dineshvallapureddy@users.noreply.github.com>
2021-10-19 14:03:34 +0530

    Update test_dih.py

--- dags/test_dih.py
+++ dags/test_dih.py
@@ -80,7 +80,6 @@ with DAG(dset["name"], default_args=default_args, schedule_interval=dset["schedu
                     #command="{} ; snowsql -a mdtplcprod.us-east-1 -u DEV_HILLTOPPERS_BI_SVC -d DEV_CDH_DB -s XDS_MAIN -w DEV_HILLTOPPERS_ANALYTICS_WH --private-key-path snowflake.pk -q  'select count(*) from {};'".format(expo,tabname)
                     taskname = "SF_{}_{}".format(dbname, tabname)
                     taskid = 'TA_' + taskname
-                    commands = "{};echo {} | kinit {}@{} && ssh -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes {}@{} '{}'".format(password,
-                        kinitprincipal, kinitdomain, kinitprincipal, edgenodehost, "{} {} {}".format(expo,commands, tabname, dbname))
+                    commands = "{};echo {} | kinit {}@{} && ssh -o StrictHostKeyChecking=no -o GSSAPIAuthentication=yes -o GSSAPIDelegateCredentials=yes {}@{} '{}'".format(password,kinitprincipal, kinitdomain, kinitprincipal, edgenodehost, "{} {} {}".format(expo,commands, tabname, dbname))
                     ssh_dih = getpodoperator(namespace, image, command, labels, taskname , taskid)
                     ssh_dih
